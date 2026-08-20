b70564111b7e388b6fe94513ec25b88347a4b748
Lemi Fayera <lemifayera1@gmail.com>
2026-08-20 23:07:33 +0300

    Time: 247 ms (70.87%), Space: 19.3 MB (69.38%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -10,10 +10,8 @@ class Solution:
             return s[left + 1:right]
 
         for i in range(len(s)):
-            # Odd-length palindrome
             odd = expand(i, i)
 
-            # Even-length palindrome
             even = expand(i, i + 1)
 
             if len(odd) > len(result):
