71a034b7708f70ec212aee6744c4643c502b717a
Lemi Fayera <lemifayera1@gmail.com>
2026-09-03 23:55:48 +0300

    Time: 0 ms (100%), Space: 53.7 MB (65.88%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+
+        // Target found
+        if (nums[mid] === target) {
+            return mid;
+        }
+
+        // Check if the LEFT half is sorted
+        if (nums[left] <= nums[mid]) {
+            // Is target inside the sorted left half?
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        // Otherwise, the RIGHT half is sorted
+        else {
+            // Is target inside the sorted right half?
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+    }
+
+    return -1;
+};
