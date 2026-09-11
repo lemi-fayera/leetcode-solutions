69f734ca4b3bd7b2da37ea94dac639e633c80c7c
Lemi Fayera <lemifayera1@gmail.com>
2026-09-11 08:54:34 +0300

    Time: 0 ms (100%), Space: 52.6 MB (98.04%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.js
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -3,13 +3,13 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-        let prefix = strs[0];
+        let pre = strs[0];
 
     for (let i = 1; i < strs.length; i++) {
-        while (!strs[i].startsWith(prefix)) {
-            prefix = prefix.slice(0, -1);
+        while (!strs[i].startsWith(pre)) {
+            pre = pre.slice(0, -1);
         }
     }
 
-    return prefix;
+    return pre;
 };
